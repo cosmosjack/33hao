d303cdf474474bec59051b2f9a50180042d69a75
jack <997823131@qq.com>
2017-09-20 17:09:47 +0800

    jack

---
+++ shop/control/atest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by 大师兄
+ * 派系: 神秘剑派
+ * 技能: zxc秒杀
+ * Date: 2017/9/20
+ * Time: 11:21
+ * QQ:  997823131
+ */
+class atestControl extends BaseHomeControl{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function indexOp(){
+//        echo 'ddd';
+//          die;
+        /*$redis = new Redis();
+        $redis->connect("127.0.0.1",6379);
+        $redis->set('ddd','aaa');
+        $res = $redis->get('ddd');
+        $mykey = $redis->get('myKey');
+        $list = $redis->getKeys("*");*/
+
+        $cache = Cache::connect('redis');
+//        $cache->set('test','hello world');
+//        $cache->set("ceshi",'ceshi');
+        echo $cache->get('test');
+        echo $cache->get('ceshi');
+
+    }
+    public function testOp(){
+        $cache = Cache::getInstance();
+        echo $cache->get('test');
+        echo $cache->get('ceshi');
+
+    }
+}
