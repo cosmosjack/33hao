8b93812d8e4c573ab682675d1ad58ac3d5701d48
jack <997823131@qq.com>
2017-09-20 09:09:42 +0800

    jack

--- member/control/login.php
+++ member/control/login.php
@@ -54,6 +54,7 @@ class loginControl extends BaseLoginControl {
             // 自动登录
             $member_info['auto_login'] = $_POST['auto_login'];
             $model_member->createSession($member_info, true);
+            session_regenerate_id(true);
             if ($_GET['inajax'] == 1){
                 showDialog('',$_POST['ref_url'] == '' ? 'reload' : $_POST['ref_url'],'js');
             } else {
